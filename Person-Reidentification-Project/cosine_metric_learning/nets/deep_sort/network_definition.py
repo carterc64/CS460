27d6fec0591e186516ad73e9758fced30e00d56f
Carterc64 <cwc00001@mix.wvu.edu>
2023-12-06 12:42:16 -0500

    Replacing slim with tf pt2

--- Person-Reidentification-Project/cosine_metric_learning/nets/deep_sort/network_definition.py
+++ Person-Reidentification-Project/cosine_metric_learning/nets/deep_sort/network_definition.py
@@ -1,6 +1,6 @@
 # vim: expandtab:ts=4:sw=4
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
+
 
 from . import residual_net
 
@@ -10,16 +10,16 @@ def create_network(images, num_classes=None, add_logits=True, reuse=None,
     nonlinearity = tf.nn.elu
     conv_weight_init = tf.truncated_normal_initializer(stddev=1e-3)
     conv_bias_init = tf.zeros_initializer()
-    conv_regularizer = slim.l2_regularizer(weight_decay)
+    conv_regularizer = tf.l2_regularizer(weight_decay)
     fc_weight_init = tf.truncated_normal_initializer(stddev=1e-3)
     fc_bias_init = tf.zeros_initializer()
-    fc_regularizer = slim.l2_regularizer(weight_decay)
+    fc_regularizer = tf.l2_regularizer(weight_decay)
 
     def batch_norm_fn(x):
-        return slim.batch_norm(x, scope=tf.get_variable_scope().name + "/bn")
+        return tf.batch_norm(x, scope=tf.get_variable_scope().name + "/bn")
 
     network = images
-    network = slim.conv2d(
+    network = tf.conv2d(
         network, 32, [3, 3], stride=1, activation_fn=nonlinearity,
         padding="SAME", normalizer_fn=batch_norm_fn, scope="conv1_1",
         weights_initializer=conv_weight_init, biases_initializer=conv_bias_init,
@@ -27,9 +27,9 @@ def create_network(images, num_classes=None, add_logits=True, reuse=None,
     if create_summaries:
         tf.summary.histogram(network.name + "/activations", network)
         tf.summary.image("conv1_1/weights", tf.transpose(
-            slim.get_variables("conv1_1/weights:0")[0], [3, 0, 1, 2]),
+            tf.get_variables("conv1_1/weights:0")[0], [3, 0, 1, 2]),
                          max_outputs=128)
-    network = slim.conv2d(
+    network = tf.conv2d(
         network, 32, [3, 3], stride=1, activation_fn=nonlinearity,
         padding="SAME", normalizer_fn=batch_norm_fn, scope="conv1_2",
         weights_initializer=conv_weight_init, biases_initializer=conv_bias_init,
@@ -37,7 +37,7 @@ def create_network(images, num_classes=None, add_logits=True, reuse=None,
     if create_summaries:
         tf.summary.histogram(network.name + "/activations", network)
 
-    network = slim.max_pool2d(
+    network = tf.max_pool2d(
         network, [3, 3], [2, 2], scope="pool1", padding="SAME")
 
     network = residual_net.residual_block(
@@ -69,10 +69,10 @@ def create_network(images, num_classes=None, add_logits=True, reuse=None,
 
     feature_dim = network.get_shape().as_list()[-1]
     print("feature dimensionality: ", feature_dim)
-    network = slim.flatten(network)
+    network = tf.flatten(network)
 
-    network = slim.dropout(network, keep_prob=0.6)
-    network = slim.fully_connected(
+    network = tf.dropout(network, keep_prob=0.6)
+    network = tf.fully_connected(
         network, feature_dim, activation_fn=nonlinearity,
         normalizer_fn=batch_norm_fn, weights_regularizer=fc_regularizer,
         scope="fc1", weights_initializer=fc_weight_init,
@@ -84,15 +84,15 @@ def create_network(images, num_classes=None, add_logits=True, reuse=None,
     features = tf.nn.l2_normalize(features, dim=1)
 
     if add_logits:
-        with slim.variable_scope.variable_scope("ball", reuse=reuse):
-            weights = slim.model_variable(
+        with tf.variable_scope.variable_scope("ball", reuse=reuse):
+            weights = tf.model_variable(
                 "mean_vectors", (feature_dim, int(num_classes)),
                 initializer=tf.truncated_normal_initializer(stddev=1e-3),
                 regularizer=None)
-            scale = slim.model_variable(
+            scale = tf.model_variable(
                 "scale", (), tf.float32,
                 initializer=tf.constant_initializer(0., tf.float32),
-                regularizer=slim.l2_regularizer(1e-1))
+                regularizer=tf.l2_regularizer(1e-1))
             if create_summaries:
                 tf.summary.scalar("scale", scale)
             scale = tf.nn.softplus(scale)
@@ -109,10 +109,10 @@ def create_network_factory(is_training, num_classes, add_logits,
                            weight_decay=1e-8, reuse=None):
 
     def factory_fn(image):
-            with slim.arg_scope([slim.batch_norm, slim.dropout],
+            with tf.arg_scope([tf.batch_norm, tf.dropout],
                                 is_training=is_training):
-                with slim.arg_scope([slim.conv2d, slim.fully_connected,
-                                     slim.batch_norm, slim.layer_norm],
+                with tf.arg_scope([tf.conv2d, tf.fully_connected,
+                                     tf.batch_norm, tf.layer_norm],
                                     reuse=reuse):
                     features, logits = create_network(
                         image, num_classes=num_classes, add_logits=add_logits,
